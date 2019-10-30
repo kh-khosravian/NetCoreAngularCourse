53fb7151d4894756c1674de3a422ffa75178941d
kh-khosravian <khosravian@outlook.com>
2019-10-30 14:44:27 -0700

    intit commit

---
+++ Dateing.API/Controllers/ValueController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dateing.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Dateing.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValueController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValueController(DataContext context)
+        {
+            this._context = context;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValue()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(v => v.Id == id);
+            return Ok(value);
+        }
+    }
+}
---
+++ Dateing.API/Models/Value.cs
@@ -0,0 +1,9 @@
+namespace Dateing.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        
+    }
+}
